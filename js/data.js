08790ab1daaad155500303d4cb4dd99990035a2b
sea-anemone17 <leehaeju09@gmail.com>
2026-06-29 11:57:18 +0900

    Add CROPS constant with crop data

---
+++ js/data.js
@@ -0,0 +1,6 @@
+export const CROPS = {
+  wheat: { id: "wheat", name: "밀", icon: "🌾" },
+  potato: { id: "potato", name: "감자", icon: "🥔" },
+  tomato: { id: "tomato", name: "토마토", icon: "🍅" },
+  flower: { id: "flower", name: "꽃", icon: "🌸" }
+};
