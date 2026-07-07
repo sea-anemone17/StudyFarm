a852b72631fa24663310131e6ec83a9a0c1c9b6d
sea-anemone17 <leehaeju09@gmail.com>
2026-07-08 08:59:39 +0900

    Rename bindNavigationEvents to bindNavigation

--- js/events/index.js
+++ js/events/index.js
@@ -1,11 +1,11 @@
-import { bindNavigationEvents } from "./navigation.js";
+import { bindNavigation } from "./navigation.js";
 import { bindKiwiEvents } from "./kiwiEvents.js";
 import { bindStudyEvents } from "./studyEvents.js";
 import { bindFarmEvents } from "./farmEvents.js";
 import { bindStorageEvents } from "./storageEvents.js";
 
 export function bindEvents(context) {
-  bindNavigationEvents(context);
+  bindNavigation(context);
   bindKiwiEvents(context);
   bindStudyEvents(context);
   bindFarmEvents(context);
